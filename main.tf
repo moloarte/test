dd88bc25305a288b5e9fad56bafdbe7b80086a81
Vaskovskij Alexandr <vaskovskyi.oleksandr@pdffiller.team>
2018-02-09 15:01:42 +0200

    added aws_security_group instance, also added tag for instance

--- main.tf
+++ main.tf
@@ -1,8 +1,29 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-40d28157"
+  ami = "ami-167f5773"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags {
+    Name = "examle"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = "example-policy"
+
+  ingress {
+    from_port =  8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
